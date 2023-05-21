07247e25527a00aef904f4462d857bcde99801d2
Alex Prc <pralig@yandex.ru>
2023-05-22 00:09:30 +0300

    mega fix v2 22.05 00.09

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
+require("dotenv").config(); // env-переменные из файла .env добавятся в process.env ; .env вгит игнор добавить
+
 const express = require("express");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
 const router = require("./routes/index");
 const { login, createUser } = require("./controllers/users");
+const bodyParser = require("body-parser");
+
 const auth = require("./middlewares/auth");
-const { validateSignUp, validateSignIn } = require('./middlewares/validator');
+const { validateSignUp, validateSignIn } = require("./middlewares/validator");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const cors = require("./middlewares/cors");
 
 const app = express();
-app.use(express.json());
+app.use(cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.post("/signin", validateSignIn, login);
 app.post("/signup", validateSignUp, createUser);
@@ -26,6 +36,8 @@ mongoose
   .catch((error) => {
     console.log("Error connecting to database:", error);
   });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use((err, req, res, next) => {
